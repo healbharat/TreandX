8c8f5bb46979786a4916303f90890e35c1ba0758
Om Sonawane <healbharatofficial@gmail.com>
2026-04-19 20:12:23 +0530

    port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,12 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  app.enableCors();
+
+  app.enableCors({
+    origin: '*', // production मध्ये specific domain देऊ शकतोस
+  });
+
   app.useGlobalPipes(new ValidationPipe());
-  
-  const port = process.env.PORT || 3001;
-  await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, '0.0.0.0'); // 🔥 IMPORTANT
+  console.log(`Application is running on port: ${port}`);
 }
 bootstrap();
